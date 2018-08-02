3a07bb49e7e920a20b3399015c28ec6555c7f335
romain.fontugne <romain.fontugne@gmail.com>
2018-08-02 11:30:51 +0900

    plot first digit distribution with theoretical distribution

--- benford.py
+++ benford.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import pandas as pd
 from matplotlib import pylab as plt
@@ -34,9 +35,58 @@ class Benford(object):
     
         self.data_first_digit = self.data.applymap(self.first_digit) 
 
-    def plot_distribution(self):
+    def plot_theory_hist(self, fig=None):
+
+        x = range(1,10)
+        benford_probability = [np.log10(1+1/xx) for xx in x ]
+        plt.figure(fig)
+        plt.plot(x, benford_probability)
+
+
+    def plot_first_digit_hist(self, title=None, fig=None):
+
+        data = self.data_first_digit
         
-        plt.figure()
-        self.data_first_digit.hist()
-        plt.title(self.dataname)
-        plt.show()
+        fig, ax = plt.subplots(num=fig)
+        data.hist(bins=np.arange(0.5, 10, 1), ax=ax,
+            density=True, stacked=True, align="mid") #, histtype="step"  )
+        plt.xticks(range(1,10))
+        plt.xlim([0,10])
+        plt.xlabel("First Digit")
+        plt.ylabel("Frequency")
+        plt.title(title)
+
+
+    def plot_raw_data_hist(self, data=None, title=None, fig=None):
+
+        data = self.data
+        
+        data.hist(bins=30, density=True, stacked=True, align="mid") #, histtype="step"  )
+        # plt.xticks(range(1,10))
+        # plt.xlim([0,10])
+        # plt.xlabel("First Digit")
+        plt.xlabel("Data Values")
+        plt.ylabel("Frequency")
+        plt.title(title)
+
+
+
+if __name__ == "__main__":
+    
+    fname = sys.argv[1]
+    title = fname.rpartition("/")[2].partition(".")[0]
+
+    ben = Benford(fname, fname)
+    ben.convert_dataset()
+
+    # Plot the first digit distribution
+    fig = 10
+    plt.figure(fig)
+    ben.plot_first_digit_hist(fig=fig, title=title)
+    ben.plot_theory_hist(fig=fig)
+    plt.tight_layout()
+    plt.savefig(title+".pdf")
+
+    ben.plot_raw_data_hist()
+    plt.tight_layout()
+    plt.savefig(title+"_raw.pdf")
